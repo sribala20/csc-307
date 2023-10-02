d0ac137c2d8c6a5cc9337725bba1f2e9ba410379
sribala20 <sribala455@gmail.com>
2023-10-01 21:47:19 -0700

    first changes

--- react-frontend/src/Table.js
+++ react-frontend/src/Table.js
@@ -13,26 +13,33 @@ function TableHeader() {
 
 function TableBody(props) {
     const rows = props.characterData.map((row, index) => {
-      return (
-        <tr key={index}>
-          <td>{row.name}</td>
-          <td>{row.job}</td>
-        </tr>
-      );
-     }
+        return (
+            <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.job}</td>
+                <td>
+                    <button onClick={() =>
+                        props.removeCharacter(index)}>
+                        Delete
+                    </button>
+                </td>
+            </tr>
+        );
+    }
     );
     return (
         <tbody>
-          {rows}
-         </tbody>
-     );
-  }
+            {rows}
+        </tbody>
+    );
+}
 
 function Table(props) {
     return (
         <table>
             <TableHeader />
-            <TableBody characterData={props.characterData}/>
+            <TableBody characterData={props.characterData}
+                removeCharacter={props.removeCharacter} />
         </table>
     );
 }
