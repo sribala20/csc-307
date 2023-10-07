cbd15f3b70ef4f07a8767f2b321ff1313ada15a2
sribala20 <sribala455@gmail.com>
2023-10-06 17:55:15 -0700

    more edits to api

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -4,11 +4,42 @@ import express from "express";
 // instance of Express and constant for port num
 const app = express(); 
 const port = 8000;
+const users = { 
+    users_list : [
+       { 
+          id : 'xyz789',
+          name : 'Charlie',
+          job: 'Janitor',
+       },
+       {
+          id : 'abc123', 
+          name: 'Mac',
+          job: 'Bouncer',
+       },
+       {
+          id : 'ppp222', 
+          name: 'Mac',
+          job: 'Professor',
+       }, 
+       {
+          id: 'yat999', 
+          name: 'Dee',
+          job: 'Aspring actress',
+       },
+       {
+          id: 'zap555', 
+          name: 'Dennis',
+          job: 'Bartender',
+       }
+    ]
+ }
 
+ 
 app.use(express.json()); //process data in JSON
 
-app.get('/', (req, res) => { //set up first API endpoint, GET requests
-    res.send('Hello World!'); 
+//api endpoint, GET requests
+app.get('/users', (req, res) => {
+    res.send(users);
 });
 
 app.listen(port, () => { //listen to incoming requests on port num
