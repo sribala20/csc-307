82e17e73c4d6f0e170033fd35b0221f4172856e9
sribala20 <sribala455@gmail.com>
2023-10-06 18:04:29 -0700

    find users by name and id

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,47 +1,77 @@
 import express from "express";
 // http middleware dispatching HTTP calls to the routes + send responses
-
+// npx nodemon backend.js
 // instance of Express and constant for port num
-const app = express(); 
+const app = express();
 const port = 8000;
-const users = { 
-    users_list : [
-       { 
-          id : 'xyz789',
-          name : 'Charlie',
-          job: 'Janitor',
-       },
-       {
-          id : 'abc123', 
-          name: 'Mac',
-          job: 'Bouncer',
-       },
-       {
-          id : 'ppp222', 
-          name: 'Mac',
-          job: 'Professor',
-       }, 
-       {
-          id: 'yat999', 
-          name: 'Dee',
-          job: 'Aspring actress',
-       },
-       {
-          id: 'zap555', 
-          name: 'Dennis',
-          job: 'Bartender',
-       }
+const users = {
+    users_list: [
+        {
+            id: 'xyz789',
+            name: 'Charlie',
+            job: 'Janitor',
+        },
+        {
+            id: 'abc123',
+            name: 'Mac',
+            job: 'Bouncer',
+        },
+        {
+            id: 'ppp222',
+            name: 'Mac',
+            job: 'Professor',
+        },
+        {
+            id: 'yat999',
+            name: 'Dee',
+            job: 'Aspring actress',
+        },
+        {
+            id: 'zap555',
+            name: 'Dennis',
+            job: 'Bartender',
+        }
     ]
- }
+}
+const findUserByName = (name) => {
+    return users['users_list']
+        .filter((user) => user['name'] === name);
+}
+const findUserById = (id) =>
+    users['users_list']
+        .find( (user) => user['id'] === id);
 
- 
 app.use(express.json()); //process data in JSON
 
 //api endpoint, GET requests
-app.get('/users', (req, res) => {
-    res.send(users);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
 
-app.listen(port, () => { //listen to incoming requests on port num
+app.get('/users', (req, res) => {
+    const name = req.query.name;
+    if (name != undefined) {
+        let result = findUserByName(name);
+        result = { users_list: result };
+        res.send(result);
+    }
+    else {
+        res.send(users);
+    }
+});
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params['id']; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+        res.status(404).send('Resource not found.');
+    } else {
+        res.send(result);
+    }
+});
+
+//listen to incoming requests on port num
+app.listen(port, () => { 
     console.log(`Example app listening at http://localhost:${port}`);
 });      
